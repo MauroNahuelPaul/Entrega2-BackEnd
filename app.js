3b941e4d4b3bc431cf4cdf2edd9474809e102698
MauroNahuelPaul <nahuelpaul2002@gmail.com>
2023-05-23 21:49:47 -0300

    se optimizo el codigo y se agrego status

--- app.js
+++ app.js
@@ -3,31 +3,22 @@ import express from 'express'
 import { manager } from "./productManager.js";
 const app = express()
 
-app.get('/', (request, response) => {
-    //html onwire! (vistas)
-    response.send('<h1>Bienvenido</h1>')
+app.get('/', (req, res) => res.status(200).json({ mensaje: 'server ok' }))
+
+app.get('/products', async (req, res) => {
+    const limit = req.query.limit
+    if (!limit) return res.status(200).json(await manager.getProducts())
+
+    const data = await manager.getProducts()
+    res.status(200).json(data.slice(0, limit))
+
 })
 
-app.get('/products', async (request, response) => {
-    const limit = request.query.limit
-    if (!limit) {
-        response.send(await manager.getProducts())
-    } else {
-        const data = await manager.getProducts()
-        let res = []
-        for (let i = 0; i < limit; i++) {
-            res.push(data[i])
-        }
-        response.send(res)
-    }
-})
+app.get('/products/:id', async (req, res) => {
+    const product = await manager.getElementById(parseInt(req.params.id))
+    if (product == null) return res.status(400).json({ error: 'El producto no existe' })
 
-app.get('/products/:id', async (request, response) => {
-    const products = await manager.getProducts()
-    const id = request.params.id
-    const product = products.find(item => item.id == id)
-    if (!id) return response.send({ error: 'El curso no existe' })
-    response.send(product)
+    res.status(200).json(product)
 })
 
 app.listen(8080, () => console.log('Server Up'))

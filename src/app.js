4a5b556cf72ee23ea14c6d6f99ea160631b74366
MauroNahuelPaul <nahuelpaul2002@gmail.com>
2023-07-02 18:55:48 -0300

    Motores de plantillas + Websocket

--- src/app.js
+++ src/app.js
@@ -2,33 +2,50 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import { Server } from 'socket.io'
+
 import productsRouter from "./routers/productsRouter.js";
-import cartsRouter from "./routers/cartsRouter.js"
+import { manager } from "./helpers/productManager.js"
+const productManager = manager
 const app = express()
 
-app.engine('handlebars', handlebars.engine())
-app.set('views', './src/views')
-app.set('view engine', 'handlebars')
+
 app.use(express.json())
 
 app.get('/', (req, res) => res.status(200).json({ mensaje: 'server ok' }))
 app.use('/conteiner', express.static('./src/public'))
+
+
 app.use('/products', function (req, res, next) {
     req.view_config = 'home',
-    next();
+        next();
 },
     productsRouter)
+
 app.use('/realtimeproducts', function (req, res, next) {
     req.view_config = 'realtimeproducts', next();
-}, 
+},
     productsRouter)
-
-
-app.use('/carts', cartsRouter)
-
 
 const serverHttp = app.listen(8080, () => console.log('Server Up'))
 const io = new Server(serverHttp)
-io.on('connection', () => {
+io.on('connection', async socket => {
     console.log('Se ha conectado')
+    socket.emit('products', await productManager.getProducts())
+
+    socket.on('product', async data => {
+        const { title, description, code, price, status, stock, category, thumbnails } = data
+        await productManager.addProduct(title, description, code, price, status, stock, category, thumbnails)
+        io.emit('products', await productManager.getProducts())
+    })
+
+    socket.on('idBorrar', async i => {
+        console.log(i)
+        await productManager.deleteProduct(parseInt(i))
+        io.emit('products', await productManager.getProducts())
+        await console.log(i)
+    })
+
 })
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
